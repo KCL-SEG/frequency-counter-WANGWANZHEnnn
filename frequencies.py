c3faa47a77048e7598b83a9b9eb6c38747986dd0
Wanzhen Wang <101357076+WANGWANZHEnnn@users.noreply.github.com>
2023-09-27 21:14:50 +0100

    Add files via upload

--- frequencies.py
+++ frequencies.py
@@ -3,18 +3,24 @@
 
 def frequencies(items):
     frequencies = {}
-    
-    
+
     for item in items:
+        # Convert the item to a string representation
         item_str = str(item)
 
-    if item_str in frequencies:
+        # Check if the item_str is already a key in the dictionary
+        if item_str in frequencies:
+            # Increment the count if the key already exists
+            frequencies[item_str] += 1
+        else:
+            # Initialize the count to 1 if the key doesn't exist
+            frequencies[item_str] = 1
 
-        frequencies[item_str] += 1
-    else:
-
-        frequencies[item_str] = 1
     return frequencies
+
+
+
+
 
    
     
