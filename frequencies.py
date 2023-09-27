ca4a07b6152bbec13cb419578ed1d674f49c1032
Wanzhen Wang <101357076+WANGWANZHEnnn@users.noreply.github.com>
2023-09-27 20:40:21 +0100

    Add files via upload

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,18 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    
+    for item in items:
+        item_str = str(item)
+
+    if item_str in frequencies:
+
+        frequencies[item_str] += 1
+    else:
+
+        frequencies[item_str] = 1
     return frequencies
+
+   
+    
